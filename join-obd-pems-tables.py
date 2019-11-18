49eec4d7cf8339f4fb8f6820c8b8926c9025ee4f
Ehsan Moradi <ehsan@Ehsans-MacBook-Pro.local>
2019-11-18 16:35:52 -0500

    Add join function, Fix load and save functions

--- join-obd-pems-tables.py
+++ join-obd-pems-tables.py
@@ -3,32 +3,57 @@
 # ### Ehsan Moradi, Ph.D. Candidate
 
 #%% [markdown]
-# ## Load required libraries
+# ## Loading required libraries
 import numpy as np
 import pandas as pd
 
 #%% [markdown]
 # ### Loading data from Excel to pandas dataframe
-def load_from_Excel(vehicle, settings):
-    input_file = vehicle + " - {0} - {1}.xlsx".format(
-        settings["input_type"], settings["input_index"]
+def load_from_Excel(vehicle, device, settings):
+    directory = (
+        "../../../Google Drive/Academia/PhD Thesis/Field Experiments/"
+        + device
+        + "/"
+        + vehicle
+        + "/Processed/"
     )
-    for device in settings["devices"]:
-        input_path = (
-            "../Field Experiments/"
-            + device
-            + "/"
-            + vehicle
-            + "/Processed/"
-            + input_file
-        )
-        sheets_dict = pd.read_excel(input_path, sheet_name=None, header=4)
-        df_dict = {}
-        for sheet_name, df in sheets_dict.items():
-            if device not in df_dict:
-                df_dict[device] = {}
-            df_dict[device][sheet_name] = df
-    return df_dict
+    input_file = vehicle + " - {0} - {1}.xlsx".format(
+        settings[device]["input_type"], settings[device]["input_index"]
+    )
+    input_path = directory + input_file
+    sheets_dict = pd.read_excel(input_path, sheet_name=None, header=1)
+    merged_df = pd.DataFrame()
+    for sheet_name, df in sheets_dict.items():
+        temp = df
+        temp["sheet"] = sheet_name
+        merged_df.append(temp)
+    return merged_df
+
+
+#%% [markdown]
+# ### Performing inner join between two pandas dataframes
+def inner_join(left, right, join_key):
+    return pd.merge(left, right, how="inner", on=join_key, sort=True, copy=True)
+
+
+#%% [markdown]
+# ### Saving the joined table back to Excel file
+def save_back_to_Excel(joined_table, vehicle, device, settings):
+    directory = (
+        "../../../Google Drive/Academia/PhD Thesis/Field Experiments/"
+        + device
+        + "/"
+        + vehicle
+        + "/Processed/"
+    )
+    output_file = vehicle + " - {0} - {1}.xlsx".format(
+        settings["output_type"], settings["output_index"]
+    )
+    output_path = directory + output_file
+    with pd.ExcelWriter(output_path, engine="openpyxl", mode="w") as writer:
+        joined_table.to_excel(writer, sheet_name="Joined Table", header=True, index=None)
+    print("{0} saved to Excel successfully!".format(vehicle))
+    return None
 
 
 #%% [markdown]
@@ -48,15 +73,26 @@ EXPERIMENTS = (
     "037 Chevrolet Malibu 2019 (1.5L TC Auto)",
     "038 Kia Optima 2012 (2.4L Auto)",
     "039 Honda Fit 2009 (1.5L Auto)",
-    "040 Chevrolet Spark 2019 (Auto)",
+    "040 Mazda 6 2009 (2.5L Auto)",
 )
 
 #%% [markdown]
-# ### Grade calculation settings
+# ### Join settings
 SETTINGS = {
-    "input_type": "NONE",
-    "output_type": "NONE",
-    "input_index": "00",
-    "output_index": "01",
-    "devices": ("Veepeak", "3DATX parSYNC Plus")
+    "Veepeak": {"input_type": "NONE", "input_index": "01"},
+    "3DATX parSYNC Plus": {
+        "input_type": "NONE",
+        "input_index": "00",
+        "output_type": "NONE",
+        "output_index": "01",
+    },
 }
+
+#%% [markdown]
+# ### Batch execution on all vehicles and their trips
+for vehicle in EXPERIMENTS:
+    # Load data corresponding to vehicle and device into a dataframe
+    left = load_from_Excel(vehicle, "Veepeak", SETTINGS)
+    right = load_from_Excel(vehicle, "3DATX parSYNC Plus", SETTINGS)
+    joined_table = inner_join(left, right, join_key="DATETIME")
+    save_back_to_Excel(joined_table, vehicle, "3DATX parSYNC Plus", SETTINGS)
