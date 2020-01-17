defcf6be3dfd0d37e71e71fc7d3328337cd85290
Ehsan Moradi <ehsan@Ehsans-iMac.local>
2020-01-17 17:25:42 -0500

    Add load, scale, model, save methods

--- 1d-cnn.py
+++ 1d-cnn.py
@@ -9,64 +9,89 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import tensorflow as tf
+from sklearn import preprocessing
 
 #%% [markdown]
-# ### Display training progress
-class ReportProgress(tf.keras.callbacks.Callback):
-    def __init__(self, df, test_split_ratio, n_epochs):
-        self.df = df
-        self.test_split_ratio = test_split_ratio
-        self.n_epochs = n_epochs
-
-    def on_train_begin(self, logs):
-        n_examples = len(self.df)
-        n_train = int((1 - self.test_split_ratio) * n_examples)
-        print(
-            "Training started on {0} out of {1} available examples.".format(
-                n_train, n_examples
-            )
-        )
-
-    def on_epoch_end(self, epoch, logs):
-        if epoch % 20 == 0 and epoch != 0 and epoch != self.n_epochs:
-            print("{0} out of {1} epochs completed.".format(epoch, self.n_epochs))
-
-    def on_train_end(self, logs):
-        print("Training finished.")
-
-
-#%% [markdown]
-# ### Load sample data from Excel to a pandas dataframe
-def load_sample_from_Excel(vehicle, settings):
+# ### Load data from Excel to a pandas dataframe
+def load_from_Excel(vehicle, settings):
     directory = (
-        "../../../Google Drive/Academia/PhD Thesis/Field Experiments/"
-        + device
-        + "/"
+        "../../../Google Drive/Academia/PhD Thesis/Field Experiments/3DATX parSYNC Plus/"
         + vehicle
         + "/Processed/"
     )
     input_file = vehicle + " - {0} - {1}.xlsx".format(
-        settings["input_type"], settings["input_index"]
+        settings["INPUT_TYPE"], settings["INPUT_INDEX"]
     )
     input_path = directory + input_file
     sheets_dict = pd.read_excel(input_path, sheet_name=None, header=0)
     df = pd.DataFrame()
     for _, sheet in sheets_dict.items():
         df = df.append(sheet)
-    df.reset_index(inplace=True, drop=True)
-    if df.shape[0] > settings["max_sample_size"]:
-        sample_size = settings["max_sample_size"]
-        df = df.sample(sample_size)
-    else:
-        sample_size = df.shape[0]
-    return df, sample_size
+    return df
+
+
+#%% [markdown]
+# ### Scale the features
+def scale(df, settings):
+    df_temp = df.copy()
+    variables = settings["FEATURES"].append(settings["DEPENDENT"])
+    scaler = preprocessing.StandardScaler().fit(df_temp[variables])
+    df_temp[variables] = scaler.transform(df_temp[variables])
+    return df_temp, scaler
+
+
+#%% [markdown]
+# ### Reverse-scale the features
+def reverse_scale(df, scaler):
+    df_temp = df.copy()
+    df_temp = np.sqrt(scaler.var_[-1]) * df_temp + scaler.mean_[-1]
+    return df_temp
+
+
+#%% [markdown]
+# ### Define the 1-D CNN model (https://machinelearningmastery.com/how-to-develop-convolutional-neural-network-models-for-time-series-forecasting)
+def define_model(features, settings):
+    model = tf.keras.Sequential()
+    model.add(
+        # no stride or padding is considered
+        tf.keras.layers.Conv1D(
+            filters=settings["N_FILTERS"],
+            kernel_size=settings["KERNEL_SIZE"],
+            activation="relu",
+            input_shape=(settings["N_STEPS"], len(features)),
+        )
+    )
+    model.add(tf.keras.layers.MaxPooling1D(pool_size=settings["POOL_SIZE"]))
+    model.add(tf.keras.layers.Flatten())
+    model.add(tf.keras.layers.Dense(settings["DENSE_LAYER_SIZE"], activation="relu"))
+    model.add(tf.keras.layers.Dropout(settings["DROP_PROP"]))
+    model.add(tf.keras.layers.Dense(1, activation="linear"))
+    return model
+
+
+#%% [markdown]
+# ### Save the predicted field back to Excel file
+def save_back_to_Excel(df, vehicle, settings):
+    directory = (
+        "../../../Google Drive/Academia/PhD Thesis/Field Experiments/3DATX parSYNC Plus/"
+        + vehicle
+        + "/Processed/"
+    )
+    output_file = vehicle + " - {0} - {1}.xlsx".format(
+        settings["OUTPUT_TYPE"], settings["OUTPUT_INDEX"]
+    )
+    output_path = directory + output_file
+    with pd.ExcelWriter(output_path, engine="openpyxl", mode="w") as writer:
+        df.to_excel(writer, header=True, index=None)
+    print("Data is saved to Excel successfully!")
+    return None
 
 
 #%% [markdown]
 # ### General settings
 plt.style.use("bmh")
 pd.options.mode.chained_assignment = None
-EXPERIMENTS = [
+EXPERIMENTS = (
     "015 VW Jetta 2016 (1.4L TC Auto)",
     "020 Honda Civic 2014 (1.8L Auto)",
     "029 Ford Escape 2006 (3.0L Auto)",
@@ -84,41 +109,48 @@ EXPERIMENTS = [
     "041 Nissan Micra 2019 (1.6L Auto)",
     "042 Nissan Rouge 2020 (2.5L Auto)",
     "043 Mazda CX-3 2019 (2.0L Auto)",
-]
+)
+EXPERIMENTS = ("015 VW Jetta 2016 (1.4L TC Auto)",)
 
 #%% [markdown]
 # ### ANN settings
 SETTINGS = {
-    "dependents": ["RPM", "FCR_LH", "PM_MGM3", "NO2_PPM", "NO_PPM", "CO2_PPM"],
-    "predicted_dependents": ["RPM_PRED", "FCR_LH_PRED", "PM_MGM3_PRED", "NO2_PPM_PRED", "NO_PPM_PRED", "CO2_PPM_PRED"],
-    "features": ["SPD_KH", "ACC_MS2", "NO_OUTLIER_GRADE_DEG"],
-    # "features": ["SPD_KH", "ACC_MS2", "NO_OUTLIER_GRADE_DEG", "RPM_PRED"]
-    "lag_order": 1,
-    "max_sample_size": 5400,
-    "test_split_ratio": 0.20,
-    "n_epochs": 200,
-    "drop_prop": 0.1,
-    "labels": {
-        "FCR_LH": "Observed Fuel Consumption Rate (L/H)",
-        "FCR_LH_PRED": "Predicted Fuel Consumption Rate (L/H)",
+    "DEPENDENT": "FCR_LH",  # other dependents to work on are RPM, PM_mGM3, NO2_mGM3, NO_mGM3, and CO2_mGM3
+    "FEATURES": ("SPD_KH", "ACC_MS2", "NO_OUTLIER_GRADE_DEG"),
+    "CROSS_VALIDATION_SPLITS": 5,
+    "N_EPOCHS": 200,
+    "DROP_PROP": 0.1,
+    "LABELS": {
         "RPM": "Observed Engine Speed (rev/min)",
         "RPM_PRED": "Predicted Engine Speed (rev/min)",
+        "FCR_LH": "Observed Fuel Consumption Rate (l/hr)",
+        "FCR_LH_PRED": "Predicted Fuel Consumption Rate (l/hr)",
+        "PM_mGM3": "Observed Particulate Matters Concentration (mg/m3)",
+        "PM_mGM3_PRED": "Predicted Particulate Matters Concentration (mg/m3)",
+        "CO2_mGM3": "Observed Carbon Dioxide Concentration (mg/m3)",
+        "CO2_mGM3_PRED": "Predicted Carbon Dioxide Concentration (mg/m3)",
+        "NO_mGM3": "Observed Nitrogen Oxide Concentration (mg/m3)",
+        "NO_mGM3_PRED": "Predicted Nitrogen Oxide Concentration (mg/m3)",
+        "NO2_mGM3": "Observed Nitrogen Dioxide Concentration (mg/m3)",
+        "NO2_mGM3_PRED": "Predicted Nitrogen Dioxide Concentration (mg/m3)",
         "SPD_KH": "Speed (Km/h)",
         "ACC_MS2": "Acceleration (m/s2)",
         "NO_OUTLIER_GRADE_DEG": "Road Grade (Deg)",
     },
-    "learning_rate": 0.001,
-    "metrics": [
-        "mean_squared_error",
-        "mean_absolute_error",
-        "mean_absolute_percentage_error",
-        "cosine_proximity",
-    ],
-    "input_type": "ANN",
-    "output_type": "ANN",
-    "input_index": "17",
-    "output_index": "19",
+    "LEARNING_RATE": 0.001,
+    "METRIC": "mean_squared_error",
+    "INPUT_TYPE": "NONE",
+    "OUTPUT_TYPE": "CNN",
+    "INPUT_INDEX": "03",
+    "OUTPUT_INDEX": "04",
 }
 
 #%% [markdown]
 # ### Batch execution on all vehicles and their trips
+for vehicle in EXPERIMENTS:
+    # Load data from Excel to a pandas dataframe
+    df = load_from_Excel(vehicle, SETTINGS)
+    # Scale the features
+    df, scaler = scale(df, SETTINGS)
+    # Save the predicted field back to Excel file
+    save_back_to_Excel(df, vehicle, SETTINGS)
