b01bf5a9206882c4b9cef9dda5f9f29249f191db
Ehsan Moradi <ehsan@Ehsans-MacBook-Pro.local>
2020-05-11 14:08:20 -0400

    Initial commit

---
+++ add-segment-id.py
@@ -0,0 +1,126 @@
+#%% [markdown]
+# ## Add experiment ID to joint obd/pems data (to detrend each experiment separately)
+# ## Experiments are sheets in original PEMS logs
+# ### Ehsan Moradi, Ph.D. Candidate
+
+#%% [markdown]
+# ### Load required packages
+import pandas as pd
+import datetime as dt
+
+#%% [markdown]
+# ### Load data from Excel to a pandas dataframe
+def load_from_Excel(vehicle, settings):
+    directory = (
+        "../../../Google Drive/Academia/PhD Thesis/Field Experiments/Veepeak/"
+        + vehicle
+        + "/Processed/"
+    )
+    input_file = vehicle + " - {0} - {1}.xlsx".format(
+        settings["INPUT_TYPE"], settings["INPUT_INDEX"]
+    )
+    input_path = directory + input_file
+    sheets_dict = pd.read_excel(input_path, sheet_name=None, header=0)
+    df = pd.DataFrame()
+    for _, sheet in sheets_dict.items():
+        df = df.append(sheet)
+    return df
+
+
+#%% [markdown]
+# ### Save the updated dataframe back to Excel file
+def save_to_excel(df, vehicle, settings):
+    directory = (
+        "../../../Google Drive/Academia/PhD Thesis/Field Experiments/Veepeak/"
+        + vehicle
+        + "/Processed/"
+    )
+    output_file = vehicle + " - {0} - {1}.xlsx".format(
+        settings["OUTPUT_TYPE"], settings["OUTPUT_INDEX"]
+    )
+    output_path = directory + output_file
+    with pd.ExcelWriter(output_path, engine="openpyxl", mode="w") as writer:
+        df.to_excel(writer, header=True, index=None)
+    print("{0} -> Data is saved to Excel successfully!".format(vehicle))
+    return None
+
+
+#%% [markdown]
+# ### General Settings
+pd.options.mode.chained_assignment = None
+EXPERIMENTS = (
+    "009 Renault Logan 2014 (1.6L Manual)",
+    "010 JAC J5 2015 (1.8L Auto)",
+    "011 JAC S5 2017 (2.0L TC Auto)",
+    "012 IKCO Dena 2016 (1.65L Manual)",
+    "013 Geely Emgrand7 2014 (1.8L Auto)",
+    "014 Kia Cerato 2016 (2.0L Auto)",
+    "015 VW Jetta 2016 (1.4L TC Auto)",
+    "016 Hyundai Sonata Sport 2019 (2.4L Auto)",
+    "017 Chevrolet Trax 2019 (1.4L TC Auto)",
+    "018 Hyundai Azera 2006 (3.8L Auto)",
+    "019 Hyundai Elantra GT 2019 (2.0L Auto)",
+    "020 Honda Civic 2014 (1.8L Auto)",
+    "021 Chevrolet N300 2014 (1.2L Manual)",
+    "022 Chevrolet Spark GT 2012 (1.2L Manual)",
+    "023 Mazda 2 2012 (1.4L Auto)",
+    "024 Renault Logan 2010 (1.4 L Manual)",
+    "025 Chevrolet Captiva 2010 (2.4L Auto)",
+    "026 Nissan Versa 2013 (1.6L Auto)",
+    "027 Chevrolet Cruze 2011 (1.8L Manual)",
+    "028 Nissan Sentra 2019 (1.8L Auto)",
+    "029 Ford Escape 2006 (3.0L Auto)",
+    "030 Ford Focus 2012 (2.0L Auto)",
+    "031 Mazda 3 2016 (2.0L Auto)",
+    "032 Toyota RAV4 2016 (2.5L Auto)",
+    "033 Toyota Corolla 2019 (1.8L Auto)",
+    "034 Toyota Yaris 2015 (1.5L Auto)",
+    "035 Kia Rio 2013 (1.6L Auto)",
+    "036 Jeep Patriot 2010 (2.4L Auto)",
+    "037 Chevrolet Malibu 2019 (1.5L TC Auto)",
+    "038 Kia Optima 2012 (2.4L Auto)",
+    "039 Honda Fit 2009 (1.5L Auto)",
+    "040 Mazda 6 2009 (2.5L Auto)",
+    "041 Nissan Micra 2019 (1.6L Auto)",
+    "042 Nissan Rouge 2020 (2.5L Auto)",
+    "043 Mazda CX-3 2019 (2.0L Auto)",
+)
+
+#%% [markdown]
+# ### Segmentation Settings
+SETTINGS = {
+    "INPUT_TYPE": "NONE",
+    "OUTPUT_TYPE": "NONE",
+    "INPUT_INDEX": "02",
+    "OUTPUT_INDEX": "03",
+}
+
+#%% [markdown]
+# ### Assign ordinal segment IDs to records
+def assign_segment_id(df):
+    id = 0
+    df["SEGMENT_ID"] = ""
+    for i in range(len(df) - 1):
+        df["SEGMENT_ID"][i] = id
+        # dt_current = dt.datetime.strptime(df["DATETIME"][i], "%Y-%m-%d %H:%M:%S")
+        # dt_next = dt.datetime.strptime(df["DATETIME"][i + 1], "%Y-%m-%d %H:%M:%S")
+        dt_current = df["DATETIME"][i]
+        dt_next = df["DATETIME"][i + 1]
+        delta = dt_next - dt_current
+        if delta.seconds > 30:
+            id += 1
+    df["SEGMENT_ID"][len(df) - 1] = id
+    return df
+
+
+#%% [markdown]
+# ### Batch execution on all vehicles and their trips
+for vehicle in EXPERIMENTS:
+    # Load data from Excel to a pandas dataframe
+    df = load_from_Excel(vehicle, SETTINGS)
+    # Assign ordinal segment IDs to records
+    df = assign_segment_id(df)
+    # Save the updated dataframe back to Excel file
+    save_to_excel(df, vehicle, SETTINGS)
+
+# %%
