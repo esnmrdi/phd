9fabc8b496e69d79c71628c93217e30a8ad1722b
Ehsan Moradi <ehsan@Ehsans-MacBook-Pro.local>
2020-05-11 14:48:44 -0400

    Disregarded sheet_names in OBD/PEMS merge

--- join-obd-pems-tables.py
+++ join-obd-pems-tables.py
@@ -17,23 +17,19 @@ def load_from_Excel(vehicle, device, settings):
         + "/Processed/"
     )
     input_file = vehicle + " - {0} - {1}.xlsx".format(
-        settings[device]["input_type"], settings[device]["input_index"]
+        settings[device]["INPUT_TYPE"], settings[device]["INPUT_INDEX"]
     )
     input_path = directory + input_file
     sheets_dict = pd.read_excel(input_path, sheet_name=None, header=0)
-    merged_df = pd.DataFrame()
-    for sheet_name, df in sheets_dict.items():
-        df["DATETIME"] = df["DATETIME"].dt.strftime("%Y-%m-%d %H:%M:%S")
-        temp = df
-        sheet_key = "SHEET_OBD" if device == "Veepeak" else "SHEET_PEMS"
-        temp[sheet_key] = sheet_name
-        merged_df = merged_df.append(temp, ignore_index=True)
-    return merged_df
+    df = pd.DataFrame()
+    for _, sheet in sheets_dict.items():
+        df = df.append(sheet, ignore_index=True)
+    return df
 
 
 #%% [markdown]
 # ### Save the joined table back to Excel file
-def save_back_to_Excel(joined_table, vehicle, device, settings):
+def save_to_excel(joined_table, vehicle, device, settings):
     directory = (
         "../../../Google Drive/Academia/PhD Thesis/Field Experiments/"
         + device
@@ -42,14 +38,14 @@ def save_back_to_Excel(joined_table, vehicle, device, settings):
         + "/Processed/"
     )
     output_file = vehicle + " - {0} - {1}.xlsx".format(
-        settings[device]["output_type"], settings[device]["output_index"]
+        settings[device]["OUTPUT_TYPE"], settings[device]["OUTPUT_INDEX"]
     )
     output_path = directory + output_file
     with pd.ExcelWriter(output_path, engine="openpyxl", mode="w") as writer:
         joined_table.to_excel(
             writer, sheet_name="Joined Table", header=True, index=None
         )
-    print("{0} saved to Excel successfully!".format(vehicle))
+    print("{0} -> Data is saved to Excel successfully!".format(vehicle))
     return None
 
 
@@ -75,15 +71,16 @@ EXPERIMENTS = (
     "042 Nissan Rouge 2020 (2.5L Auto)",
     "043 Mazda CX-3 2019 (2.0L Auto)",
 )
+
 #%% [markdown]
 # ### Join settings
 SETTINGS = {
-    "Veepeak": {"input_type": "NONE", "input_index": "01"},
+    "Veepeak": {"INPUT_TYPE": "NONE", "INPUT_INDEX": "04"},
     "3DATX parSYNC Plus": {
-        "input_type": "NONE",
-        "input_index": "00",
-        "output_type": "NONE",
-        "output_index": "01",
+        "INPUT_TYPE": "NONE",
+        "INPUT_INDEX": "00",
+        "OUTPUT_TYPE": "NONE",
+        "OUTPUT_INDEX": "01",
     },
 }
 
@@ -93,9 +90,12 @@ for vehicle in EXPERIMENTS:
     # Load data corresponding to vehicle and device into a dataframe
     left = load_from_Excel(vehicle, "Veepeak", SETTINGS)
     right = load_from_Excel(vehicle, "3DATX parSYNC Plus", SETTINGS)
+    left["DATETIME"] = left["DATETIME"].dt.strftime("%Y-%m-%d %H:%M:%S")
+    right["DATETIME"] = right["DATETIME"].dt.strftime("%Y-%m-%d %H:%M:%S")
     # Join tables based on DATETIME as the common column
-    joined_table = pd.merge(left, right, how="inner", on="DATETIME", sort=True)
+    joined_table = pd.merge_ordered(left, right, how="inner", on="DATETIME")
+    joined_table["DATETIME"] = pd.to_datetime(joined_table["DATETIME"], format='%Y-%m-%d %H:%M:%S')
     # Save the joined table back to Excel file
-    save_back_to_Excel(joined_table, vehicle, "3DATX parSYNC Plus", SETTINGS)
+    save_to_excel(joined_table, vehicle, "3DATX parSYNC Plus", SETTINGS)
 
 # %%
